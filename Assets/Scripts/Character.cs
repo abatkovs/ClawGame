16e288a22b0a1a8203a81305ca71d106fdf22615
Dragoons <arnolds.batkovskis@gmail.com>
2024-01-04 22:21:08 +0200

    Simple movement
    WASD and Joystick movement

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,64 @@
+using System;
+using Scripts.Input;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float gravity = -9.8f;
+    [SerializeField] private Vector3 movementVelocity;
+
+    private CharacterController _characterController;
+    private float _verticalVelocity;
+
+    private Camera _mainCam;
+
+    private InputReader _input;
+    
+    private void Awake()
+    {
+        _mainCam = Camera.main;
+    }
+
+    private void Start()
+    {
+        _input = InputReader.Instance;
+        if (_characterController == null) _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        SetVelocity(_input.MovementValue);
+    }
+
+    private void FixedUpdate()
+    {
+        CalculateMovement();
+            
+        if (!_characterController.isGrounded) 
+            _verticalVelocity = gravity;
+        else
+            _verticalVelocity = gravity * 0.3f;
+            
+        movementVelocity += Vector3.up * (_verticalVelocity * Time.deltaTime);
+            
+        _characterController.Move(movementVelocity);
+    }
+        
+    public void SetVelocity(Vector3 velocity)
+    {
+        movementVelocity.Set(velocity.x, 0, velocity.y);
+    }
+
+    public void SetCharacterController(CharacterController controller)
+    {
+        _characterController = controller;
+    }
+
+    private void CalculateMovement()
+    {
+        //Align to camera
+        movementVelocity = Quaternion.Euler(0, _mainCam.transform.eulerAngles.y, 0) * movementVelocity;
+        movementVelocity *= speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,95 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Scripts.Input
+{
+    public class InputReader : MonoBehaviour, Controls.IPlayerActions
+    {
+        public static InputReader Instance;
+    
+        [SerializeField] private Camera mainCam;
+        public Vector2 MovementValue { get; private set; }
+        public Vector2 JoystickAim { get; private set; }
+        public Vector2 MousePosition { get; private set; }
+    
+        public bool IsUsingGamePadAiming { get; private set; }
+    
+        private Controls _controls;
+        private bool _isMouseOverUI;
+
+        public event Action OnShootButtonPress;
+        public event Action OnShootButtonReleased;
+        public event Action OnDashButtonPress;
+        public event Action OnPauseButtonPress;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private void Start(){
+            _controls = new Controls();
+            _controls.Player.SetCallbacks(this); //bind input class to controls class
+
+            _controls.Player.Enable();
+        }
+
+        private void OnDestroy() {
+            if ( _controls == null) { return; }
+            _controls.Player.Disable();
+        }
+    
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            MovementValue = context.ReadValue<Vector2>();
+        }
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+            if(_isMouseOverUI) return;
+            if (context.performed) OnShootButtonPress?.Invoke();
+            if (context.canceled) OnShootButtonReleased?.Invoke();
+        }
+
+        public void OnAim(InputAction.CallbackContext context)
+        {
+            if(context.ReadValue<Vector2>() == Vector2.zero) return;
+            JoystickAim = context.ReadValue<Vector2>();
+            IsUsingGamePadAiming = true;
+        }
+
+        public void OnMousePosition(InputAction.CallbackContext context)
+        {
+            MousePosition = context.ReadValue<Vector2>();
+            IsUsingGamePadAiming = false;
+        }
+
+        public void OnAnyKey(InputAction.CallbackContext context)
+        {
+            IsUsingGamePadAiming = false;
+        }
+
+        public void OnDash(InputAction.CallbackContext context)
+        {
+            if(!context.performed)return;
+            OnDashButtonPress?.Invoke();
+        }
+
+        public void OnPause(InputAction.CallbackContext context)
+        {
+            if (!context.performed) return;
+            OnPauseButtonPress?.Invoke();
+        }
+
+        public void MouseEntersUI()
+        {
+            _isMouseOverUI = true;
+        }
+
+        public void MouseLeavesUi()
+        {
+            _isMouseOverUI = false;
+        }
+    }
+}
