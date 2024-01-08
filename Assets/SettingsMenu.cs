0afcbd6d70bbc82cb84d5bd4bbce8db6fa18f509
Justin Chen <Chen221@usc.edu>
2024-01-08 13:39:54 -0800

    Working volume slider and fullscreen menu
    I think I might do a quality changer next

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume) 
+    {
+        Debug.Log("Current volume: " + volume);
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetFullscreen(bool isFullscreen) {
+        Screen.fullScreen = isFullscreen;
+    }
+}
