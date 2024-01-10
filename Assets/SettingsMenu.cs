5d5b6f9c80c2283af7b79e85a2be045cea268c6c
Justin Chen <Chen221@usc.edu>
2024-01-10 12:07:21 -0800

    UI elements
    Added resolution dropdown and quality control

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -2,11 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public TMPro.TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
 
-    public void SetVolume(float volume) 
+    void Start() {
+        //Gather info on starting resolutions
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        //currentResolutionIndex selects correct one at the start
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++) {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height) {
+                currentResolutionIndex = i;
+            }
+
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+        
+
+    }
+
+    public void SetVolume(float volume)
     {
         Debug.Log("Current volume: " + volume);
         audioMixer.SetFloat("Volume", volume);
@@ -14,5 +44,14 @@ public class SettingsMenu : MonoBehaviour
 
     public void SetFullscreen(bool isFullscreen) {
         Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetQuality(int qualityIndex) {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetResolution(int resolutionIndex) {
+        Resolution res = resolutions[resolutionIndex];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
     }
 }
